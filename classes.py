52a69ea91237a156be1442ab4fdcc7a6609aa540
thib ters <43893686+ashesilver@users.noreply.github.com>
2021-03-07 16:59:33 +0100

    les mondes (et Top&Bot)

--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ class Node(object):
 	"""docstring for Node"""
 	
 
-	def __init__(self, ar, succtup):
+	def __init__(self, ar, succtup = None):
 		if ar != 0:
 			self.succ = succtup
 			self.ar = ar
@@ -17,6 +17,13 @@ class Node(object):
 		if self.ar == 1 :
 			return self.succ.__repr__()
 
+class Atom(Node):
+	"""docstring for Atom"""
+	def __init__(self):
+		super(Atom, self).__init__(0)
+
+
+### représentation des formules
 class And(Node):
 
 	def __init__(self, arg, arg2):
@@ -48,18 +55,95 @@ class Not(Node):
 
 	def __repr__(self):
 		return '!('+self.succ.__repr__()+')'
-		
-class Variable(Node):
+
+
+class Variable(Atom):
 	"""docstring for Variable"""
 	def __init__(self, name):
 		self.name = name
+		self._value = False
+
+	@property
+	def value(self):
+		return self._value
+
+	@value.setter
+	def value(self, val):
+		self._value = val
+
+	@value.deleter
+	def value(self):
+		self._value = False
 
 	def __repr__(self): 
 		return self.name.upper()
+
+class Top(Atom):
+	"""docstring for Top"""
+	def __init__(self):
+		super(Top, self).__init__()
+		self.value = True
 		
+class Bot(Atom):
+	"""docstring for Top"""
+	def __init__(self):
+		super(Bot, self).__init__()
+		self.value = False
 
+### représentation des mondes	
+		
+class World(Node):
+	"""docstring for World"""
+	def __init__(self, name = None):
+		
+		super(World, self).__init__(0)
+		del self.succ
+
+		self.name = name
+		self._sons = []
+		self._vars = []
+
+	@property
+	def sons(self):
+		return self._sons
+
+	@sons.setter
+	def sons(self, value):
+		self.ar+=1
+		if type(value) == str :
+			self._sons.append(World(value))
+		elif type(value) != World :
+			self._sons.append(value)
+		else :
+			self._sons.append(World(str(value)))
+		return
+
+	@sons.deleter
+	def sons(self):
+		if self.ar < 1 :
+			return
+		self.ar -= 1
+		self._sons.pop()
+	
+	@property
+	def vars(self):
+		return self._vars
+	
+
+def delete(model, world):
+	if model == world :
+		del model
+	for x in model._sons :
+		if x == world :
+			world._sons = []
+			del world 
+
+
+
+
+
+### TESTZONE
 Node.classVar = Variable("undefined")
-
 
 #formumle = X => ( Y v X )
 
@@ -69,4 +153,4 @@ form1 = Imp(X,And(Y,X))
 
 form2 = Imp(Or(And(A,B),Not(A)),And(Imp(C,A),Not(C)))
 
-print(form1,"\n",form2)
+print(form1,"\n",form2, "\n")
