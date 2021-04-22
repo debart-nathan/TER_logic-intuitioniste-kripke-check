5a0646ecb54e4b92d5b74a1089bbb356f61b5757
Debart Nathan <nathan.debart@etu.umontpellier.fr>
2021-04-22 14:53:52 +0200

    Replace main.py

--- main.py
+++ main.py
@@ -1,23 +1,88 @@
 from classes import *
 from savefilesystem import *
+from graph_view import TreeViewer,TreeWrapper
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
-formula=Not(Top())
+formula=Imp(Not(Variable("a")),Or(Variable("undefined"),Variable("b")))
+select=formula
+
+class FormuleWrapper(TreeWrapper):
+	def children(self,node):
+		if not type(node) in [Variable,Top,Bot] :
+			return [succ for succ in node.succ]
+		else:
+			return None
+	def label(self, node):
+		if node.name != 'undefined':
+			return node.name
+		elif node != Node.classVar:
+			return ''
+		else:
+			return None
+	def onClick(self,node):
+		global select
+		select=node
+		viewer.drawTree(formula)
+	
+	def bg(self, node):
+		if node==select:
+			return 'yellow2'
+		return 'gray77'
+
+
+	
+		
+
+
+
+def replacerec(father,selected,elem):
+	if not type(father) in [Variable,Top,Bot] :
+		if selected in father.succ:
+			father.succ=[elem if x==selected else x for x in father.succ]
+			return True
+		else:
+			for succ in father.succ:
+				if replacerec(succ,selected,elem):
+					return True
+			return False
+
+	return False
+				
+				
+
+def replace(selected,elem):
+	global select#temporary
+	global formula#temporary
+	if formula==selected:
+		formula=elem#temporary
+		select=elem#temorary
+		print(formula)
+		return True
+	else:
+		if replacerec(formula,selected,elem):
+			select=elem#temporary
+			print(formula)
+			return True
+		else:
+			return False
+
+
 
 def getlistvarrec(current):
 	listvar=[]
 	
 	if type(current)==Variable:
-		if current!=Node.classVar:
-			listvar.append(current)
+		if current.name!='undefined':
+			listvar.append(current.name)
 	
 	elif type(current)!=Top and type(current)!=Bot :
-	   for succ in current.succ:
-		   varsuc=getlistvarrec(succ)
-		   if not (varsuc  in listvar):
-			   listvar.extend(varsuc) 
+		for succ in current.succ:
+			varsuc=getlistvarrec(succ)
+			for var in varsuc:
+				if not (var  in listvar):
+					listvar.append(var)
 	return listvar
 
 def getlistvar():
@@ -25,12 +90,14 @@ def getlistvar():
 	root=formula
 	
 	if type(root)==Variable:
-		if root!=Node.classVar:
-			listvar.append(root)
+		if root.name!='undefined':
+			listvar.append(root.name)
 	elif type(root)!=Top and type(root)!=Bot:
-
-	   for succ in root.succ:
-		   listvar.extend(getlistvarrec(succ))
+		for succ in root.succ:
+			varsuc=getlistvarrec(succ)
+			for var in varsuc:
+				if not (var  in listvar):
+					listvar.append(var)
 	return listvar
 
 
@@ -38,41 +105,90 @@ def getlistvar():
 
 def createVar():
 	if varname.get()!='':
-		name= varname.get()
-		#select=getSelect
-		var = Variable(name)
-		#replace(select,var)
-		return messagebox.showinfo('message',f'Var {name} created.')
+		if varname.get()!= 'undefined':
+			name= varname.get().lower()
+			#select=getSelect()
+			var = Variable(name)
+			if replace(select,var):
+				viewer.drawTree(formula)
+				selectvar.delete(0,tk.END)
+				listvar2= getlistvar()
+				for var in listvar2:
+					selectvar.insert(tk.END,var.lower())
+
+				return messagebox.showinfo('message',f'Var {name} created.')
+			else:
+				return messagebox.showinfo('ERROR:',f'Could not find selected node in formula.')
+		else:
+			return messagebox.showinfo('message',f'undefined is not a valid variable name')
 	elif  len(selectvar.curselection())==1:
-		var=listvar[selectvar.curselection()[0]]
+		#select=getSelect()
+		var=Variable(listvar[selectvar.curselection()[0]])
 		name=var.name
- 		#replace(select,var)
-		return messagebox.showinfo('message',f'Var {name} assigned.')
+		if replace(select,var):
+			viewer.drawTree(formula)
+			return messagebox.showinfo('message',f'Var {name} assigned.')
+		else:
+			return messagebox.showinfo('ERROR:',f'Could not find selected node in formula.')
 	elif len(selectvar.curselection()) != 0:
 		return messagebox.showinfo('message',f'please select only one var')
 	else:
 		return messagebox.showinfo('message',f'please select item or enter value')
 
-fenetre = tk.Tk()
-fenetre.title("Logique intuitioniste")
+###############################################
+#Windows init
+###############################################
+windows = tk.Tk()
+windows.title("Logique intuitioniste")
 
-mainframe = ttk.Frame(fenetre, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-fenetre.columnconfigure(2, weight=1)
-fenetre.rowconfigure(2, weight=1)
+mainFrame=tk.Frame(windows)
+mainFrame.pack(fill='both',expand=True)
 
-varname= tk.Entry(mainframe)
+###############################################
+	#formule viewver
+###############################################
+graphe=tk.Frame(mainFrame, height=500, width=800)
+graphe.grid(row=1,column=1,pady=10,padx=10,sticky=(tk.NW))
+
+fwrap=FormuleWrapper()
+viewer= TreeViewer(fwrap,graphe,formula)
+
+###############################################
+	#toolbox
+###############################################
+boxoutils = ttk.Notebook(mainFrame,width=200, height=400)
+boxoutils.grid(row=1,column=2,pady=10,padx=10,sticky=(tk.NE))
+
+###############################################
+		#outils
+###############################################
+fOutils=ttk.Frame(boxoutils,width=200, height=400)
+fOutils.pack(fill='both',expand=True)
+boxoutils.add(fOutils,text='Outils')
+
+
+
+################################################
+		#Custom Variable creation
+################################################
+
+fVariable=ttk.Frame(boxoutils,width=200, height=400)
+fVariable.pack(fill='both',expand=True)
+boxoutils.add(fVariable,text='variable')
+
+
+varname= tk.Entry(fVariable)
 varname.grid(row=1,column=1)
 
 listvar= getlistvar()
 
-selectvar=tk.Listbox(mainframe,selectmode=tk.MULTIPLE,yscrollcommand=True)
+selectvar=tk.Listbox(fVariable,selectmode=tk.MULTIPLE,yscrollcommand=True)
 for var in listvar:
-	selectvar.insert(tk.END,var.name)
+	selectvar.insert(tk.END,var.lower())
 selectvar.grid(row=2,column=1)
 
-setvar= ttk.Button(mainframe, text='assigner', command=createVar).grid(row=1,column=2)
+setvar= ttk.Button(fVariable, text='assigner', command=createVar).grid(row=1,column=2)
 
- 
+ #########################
 
-fenetre.mainloop()
+windows.mainloop()
